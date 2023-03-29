5bbb58460d3267948134f5ee094ff09aac6d3945
D1mkadon <dima0938157970@gmail.com>
2023-03-29 21:33:31 +0200

    deleteRecoil

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,15 @@
 import "@/styles/globals.css";
 import { AnimatePresence } from "framer-motion";
 import { useRouter } from "next/router";
-import { RecoilRoot } from "recoil";
+
 import { CartProvider } from "../../context/CartContext";
 export default function App({ Component, pageProps }) {
   const router = useRouter();
   return (
-    <RecoilRoot>
-      <AnimatePresence mode="wait">
-        <CartProvider>
-          <Component {...pageProps} key={router.route} />
-        </CartProvider>
-      </AnimatePresence>
-    </RecoilRoot>
+    <AnimatePresence mode="wait">
+      <CartProvider>
+        <Component {...pageProps} key={router.route} />
+      </CartProvider>
+    </AnimatePresence>
   );
 }
